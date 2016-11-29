f5c8ddbd0d21f40ff3e6428bd0223c96e199f699
Alvaro Ortiz Sierra <alvaro@ortiz.se>
2016-11-29 23:42:24 +0100

    message goes here

--- slutprojekt/functions.js
+++ slutprojekt/functions.js
@@ -2,10 +2,16 @@ function myFunction () {
 //	document.getElementById("film").innerHTML = "Hello World";
 	$(document).ready(function() {
 		$.getJSON("films.json", function(data) {
-			$.each(data.movieInfo, function() {
-				$("film").append("<li>Car ID: " + this[movieInfo[0].synopsis]);
+			console.log(data);
+			var items = data.items.map(function (item) {
+				return item.key + ': ' + item.value;
+			});
+			if (items.length) {
+				var content = '<li>' + items.join('</li><li>') + '</li>';
+				var list = $('<ul />').html(content);
+				$("#film").append(list);
+			}
 
-        	});         
-    	});     
+    	});
 	});
 }
