986ca57600181263de4ecf1b06399b27b2e7a8b7
Alvaro Ortiz Sierra <alvaro@ortiz.se>
2016-12-01 04:49:36 +0100

    Last update before submission

---
+++ slutprojekt/README.md
@@ -0,0 +1,32 @@
+# Final Assignment
+
+#### Technologies used for this school project
+
+- HTML and CSS: Used for structure and styling of the project.
+- Bootstrap v3: Used for creating a responsive menu and also a carousel containg my most anticipated upcoming sci-fi movies.
+- JQuery: Used for handling AJAX/JSON, iteration, element content manipulation
+- JavaScript: Used for hiding and making elements visible again and also for handling the contact form.
+- Git: Used for version control.
+
+#### Description of the project
+
+A static page that fetches data from a JSON-formatted database containing information about upcoming sci-fi movies and uses a bootstrap-enabled carousel to walk the user through them. The data has been arbitrarily collected in accordance with my own taste. There is also a stylized form that will be used in the future to send me an e-mail via a contact form.
+
+##### Tested Resolutions
+- 1920 x 1080, Desktop (Chrome 54.0.2840.59 on Ubuntu Linux 16.10)
+- xxhdpi, 640 x 360dp (Chrome 54.0.2840.85 on Nexus 5, Android 6.0.1).
+
+##### School
+Lernia Yrkeshögskola, Stockholm, Sweden.
+
+#### Course
+HTML/CSS/JavaScript and Webstandards.
+
+#### Program
+JavaScript FrontEnd developer, YHJUST16.
+
+#### Author
+Alvaro Ortiz Sierra
+
+#### Project page
+https://alvaro-ortiz.github.io/slutprojekt/
--- slutprojekt/functions.js
+++ slutprojekt/functions.js
@@ -1,30 +1,76 @@
-function myFunction () {
+//Fetch JSON data and format the carousel on the page
+function handleJSON () {
 //	document.getElementById("film").innerHTML = "Hello World";
 	$(document).ready(function() {
 		$.getJSON("films.json", function(data) {
-			console.log(data);
+//			console.log(data);
 			var movies = data.movieInfo;
 
 			var items = data.movieInfo.map(function (item) {
-				return formatMovie(item, item.indexOf());//item.filmTitle + ': ' + item.synopsis;
+				return formatMovie(item);
 			});
-			console.log(items);
-/*			$.each(items) {
-				var content = '"<li data-target="#carousel1" data-slide-to='
-				content += '>' + items.join('</li><li>') + '</li>';
-				var list = $('<ol />').html(content);
-				$("#film").append(list);
-			}*/
-
+//			console.log(items);
+			var indicators = '<ol class="carousel-indicators">';
+			var slides = '<div class="carousel-inner" role="listbox">';
+			$.each(items, function(index, value) {
+				indicators += '<li data-target="#carousel1" data-slide-to="' + index + '"';
+				if (index == 0) {
+					indicators += ' class="active"';
+					value = value.replace('class="item"','class="item active"');
+				}
+				slides += value;
+				indicators += '></li>';
+//				console.log(index + ":" + value);
+			});
+			indicators += "</ol>";
+			slides += "</div>";
+//			console.log(slides);
+			var controls = '<a class="left carousel-control" href="#carousel1" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a>';
+			controls += '<a class="right carousel-control" href="#carousel1" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>';
+			var content = '<div id="carousel1" class="carousel slide" data-ride="carousel" data-interval="10000">' + indicators + slides + controls;
+			document.getElementById("welcome").style.visibility = "hidden";
+			document.getElementById("welcome").style.display = "none";
+			document.getElementById("formDiv").style.visibility = "hidden";
+			document.getElementById("formDiv").style.display = "none";
+			$("#film").html(content);
     	});
 	});
 }
 
-function formatMovie(item, i) {
-	var m = "<div class=\"item";
-	if (i == 0) {
-		m += " active";
-	}
-	m += "\">" + item.FilmTitle + "</div>";
+//Show movie title and synopsis inside carousel
+function formatMovie(item) {
+	var obj = new Object();
+	var m;
+	$.each(item, function(key, value) {
+		obj[key] = value;
+	});
+//	console.log(item);
+	m = "<div class=\"item\">";
+	m += '<img src="' + obj.poster + '" alt="' + obj.filmTitle + '">';
+	m += '<div class="carousel-caption" style="background-color:rgba(0, 0, 0, 0.5)"><h3><a href="http://www.imdb.com/title/' + obj.imdb + '/" target="_blank">' + obj.filmTitle + "</a></h3>";
+	m += '<h6>' + obj.releaseDate + '<br><a href="https://youtu.be/' + obj.trailer + '" target="_blank">[ Trailer ]</a></h6>';
+	m += obj.synopsis; 
+	m += "</div></div>";
 	return m;
+}
+
+//Reset page to default
+function resetPage() {
+	document.getElementById("film").innerHTML = "";
+	document.getElementById("welcome").style.visibility = "visible";
+	document.getElementById("welcome").style.display = "block";
+	document.getElementById("formDiv").style.visibility = "hidden";
+	document.getElementById("formDiv").style.display = "none";
+}
+
+function toggleElement(e) {
+	$(e).toggle();
+}
+
+function showForm() {
+	document.getElementById("film").innerHTML = "";
+	document.getElementById("welcome").style.visibility = "hidden";
+	document.getElementById("welcome").style.display = "none";
+	document.getElementById("formDiv").style.visibility = "visible";
+	document.getElementById("formDiv").style.display = "block";
 }
